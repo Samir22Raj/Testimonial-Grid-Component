fde0d552f8fe38943b375c8ea85091a0d0258eb2
Samir Rajkarnikar <samirrajkarnikar9@gmail.com>
2024-03-19 13:59:53 +0545

    Completed the project using grid layout

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["*"],
+	theme: {
+		colors: {
+			moderateViolet: "hsl(263, 55%, 52%)",
+			veryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+			veryDarkBlackishBlue: "hsl(219, 29%, 14%)",
+			White: "hsl(0, 0%, 100%)",
+			lightGray: "hsl(0, 0%, 81%)",
+			lightGrayishBlue: "hsl(210, 46%, 95%)",
+		},
+		fontFamily: {
+			Barlow: "Barlow+Semi+Condensed",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
